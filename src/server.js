c51d0ff343f33224623aa5c8efa6ed7ca2962e8d
Ruan Silva <ruan.fox.2013@gmail.com>
2023-08-14 16:47:46 -0300

    criando-base

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const routes = require('./routes');
+
+const server = express();
+mongoose.connect('mongodb+srv://RuanSilva:Ruansilva@projectapi.0ebdwkh.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true });
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
