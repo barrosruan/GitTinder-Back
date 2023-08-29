d0486b2b42349f8d2938a891b7f53b1b319acd22
Ruan Silva <ruan.fox.2013@gmail.com>
2023-08-29 20:06:55 -0300

    correção-cod

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,47 +1,46 @@
-const axios = require('axios');
-const Dev = require('../models/Dev');
+const axios = require("axios");
+const Dev = require("../models/Dev");
 
 module.exports = {
+  async index(req, res) {
+    const { user } = req.headers;
 
-    async index(req, res) {
-        const { user } = req.headrs;
+    const loggedDev = await Dev.findById(user);
 
-        const loggedDev = await Dev.findById(user);
+    console.log(loggedDev);
 
-        const users = await Dev.find({
-            $and: [
-                { _id: { $ne: user } },
-                { _id: { $nin: loggedDev.likes } },
-                { _id: { $nin: loggedDev.dislikes } },
-            ],
-        })
-        return res.json(users);
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedDev.likes } },
+        { _id: { $nin: loggedDev.dislikes } },
+      ],
+    });
+    return res.json(users);
+  },
 
-    },
+  async store(req, res) {
+    const { username } = req.body;
 
-    async store(req, res) {
+    const userExists = await Dev.findOne({ user: username });
 
-        const { username } = req.body;
-
-        const userExists = await Dev.findOne({ user: username });
-
-        if (userExists) {
-            return res.json(userExists)
-        }
-
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-
-
-        const { name, bio, avatar_url: avatar } = response.data;
-
-        const dev = await Dev.create({
-            name: name ?? username,
-            user: username,
-            bio,
-            avatar: avatar,
-        })
-
-
-        return res.json(dev);
+    if (userExists) {
+      return res.json(userExists);
     }
+
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+
+    const { name, bio, avatar_url: avatar } = response.data;
+
+    const dev = await Dev.create({
+      name: name ?? username,
+      user: username,
+      bio,
+      avatar: avatar,
+    });
+
+    return res.json(dev);
+  },
 };
